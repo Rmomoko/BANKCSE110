90fa693942fb2d03aa074454ae62b180bda5342e
Yuxiao <zhouyuxiao89@yahoo.com>
2014-11-22 19:05:00 -0800

    Unit test

---
+++ app/src/main/java/rmomoko/cse110bank/Object/CheckingAccount.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kobeguo on 2014/11/2.
+ */
+package rmomoko.cse110bank.Object;
+public class CheckingAccount extends Account {
+
+
+
+}
---
+++ app/src/main/java/rmomoko/cse110bank/Object/SavingAccount.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kobeguo on 2014/11/2.
+ */
+package rmomoko.cse110bank.Object;
+public class SavingAccount extends Account {
+    private double interest;
+
+
+}
