ef08361086e6e1c9b503bc48af225434214d0923
yenhsia922 <yelin@ucsd.edu>
2014-12-09 23:24:58 -0800

    Add Comments

--- app/src/main/java/rmomoko/cse110bank/CustomerAccountActivity.java
+++ app/src/main/java/rmomoko/cse110bank/CustomerAccountActivity.java
@@ -1,3 +1,10 @@
+/**
+ * Team Name: Orange Chicken
+ *  File Name: CustomerAccountActivity.java
+ *  Description: Display Customer's account information, and the customer can click on each button to continue the activity.
+ *
+ */
+
 package rmomoko.cse110bank;
 import android.content.Intent;
 import android.animation.Animator;
@@ -39,27 +46,47 @@ import rmomoko.cse110bank.Object.User;
 import rmomoko.cse110bank.Object.Account;
 
 /**
+ * Name:            CustomerAccountActivity
+ * Purpose:        Display customer's account information when customers login.
+ * Description:  Get the customer's checking and saving balance from database.
+ *                      Create customer's account activity button, and perform the activity.
+ *                      Check the account
  * Created by Yuxiao on 11/9/2014.
  */
 public class CustomerAccountActivity extends Activity{
 
+    /* TextView to show the balance of account amount */
     private TextView checkingNumber;
     private TextView savingNumber;
+
+    /* Data type from Parse */
     private User user;
     private CheckingAccount userCheckAccount;
     private SavingAccount userSaveAccount;
+
+    /* Decimal to display amount */
     private DecimalFormat f = new DecimalFormat("##.00");
 
+    /**
+         * Name:           OnCreate
+        * Purpose:        Create the layout of Customer Account Information page
+        * Description:  Create the UI of CustomerAccountActivity page.
+        *                      Get data from database to display the amount of accounts.
+        *                       Create all the buttons to do all the customer account activities.
+        */
     public void onCreate(Bundle savedInstanceState) {
-
+        /* Create the layout of CustomerAccountActivity page*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_account);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
         checkingNumber = (TextView) findViewById(R.id.cus_check_number);
         savingNumber = (TextView) findViewById(R.id.cus_save_number);
 
+        /* get the User information of the customer */
         user = (User)ParseUser.getCurrentUser();
 
+        /* get the checking account / saving account  information of the customer */
         userCheckAccount = user.getCheckingAccount();
         userSaveAccount = user.getSavingAccount();
 
@@ -69,9 +96,12 @@ public class CustomerAccountActivity extends Activity{
                     userSaveAccount.fetchInBackground(new GetCallback<SavingAccount>() {
                         public void done(SavingAccount object, ParseException e) {
                             if (e == null) {
+                                /* Check if account is not closed */
                                 if(!userCheckAccount.isClosed())
+                                    /* display the balance */
                                     checkingNumber.setText("$ " + f.format(userCheckAccount.getBalance()));
                                 else
+                                    /* account closed */
                                     checkingNumber.setText("Account Closed");
                                 if(!userSaveAccount.isClosed())
                                     savingNumber.setText("$ " + f.format(userSaveAccount.getBalance()));
@@ -87,39 +117,51 @@ public class CustomerAccountActivity extends Activity{
             }
         });
 
+        /* create print summary button */
         Button printSumButton = (Button) findViewById(R.id.cus_print_summary_button);
         printSumButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                /* Change page to summary */
                 pageToSummary(view);
             }
         });
 
+        /* create transfer money button */
         Button transferButton = (Button) findViewById(R.id.cus_transfer_button);
         transferButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                /* Change page to transfer money */
                 pageToTransfer(view);
             }
         });
 
+        /* create close checking account button */
         Button closeCheckButton = (Button) findViewById(R.id.cus_close_check_button);
         closeCheckButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                /* get user information */
                 user = (User)ParseUser.getCurrentUser();
+                /* get account information */
                 userCheckAccount = user.getCheckingAccount();
                 userCheckAccount.fetchInBackground(new GetCallback<CheckingAccount>() {
                     public void done(CheckingAccount object, ParseException e) {
                         if (e == null) {
+                            /* if the account has not been closed */
                             if(object.isClosed() == false) {
+                                /* perform close account */
                                 object.closeAccount();
+                                /* show confirm close message */
                                 checkingNumber.setText("Account Closed");
+                                /* temporarily message */
                                 Toast.makeText(CustomerAccountActivity.this, "Your account is closed now!", Toast.LENGTH_SHORT).show();
                                 check();
                             }
                             else
                             {
+                                /* confirm closed already, no action needed */
                                 Toast.makeText(CustomerAccountActivity.this, "Your account is already closed!", Toast.LENGTH_SHORT).show();
                             }
                         } else {
@@ -130,6 +172,7 @@ public class CustomerAccountActivity extends Activity{
             }
         });
 
+        /* almost the same as close checking button  above*/
         Button closeSaveButton = (Button) findViewById(R.id.cus_close_saving_button);
         closeSaveButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -158,6 +201,12 @@ public class CustomerAccountActivity extends Activity{
         });
     }
 
+    /**
+        * Name:           check
+        * Purpose:        Check if the user close both checking and saving account goto login page.
+        * Description:  Check both account is closed or not.
+        *                       Force the customer logout and goto login page.
+        */
     public void check()
     {
         if(userCheckAccount.isClosed() && userSaveAccount.isClosed())
@@ -166,7 +215,11 @@ public class CustomerAccountActivity extends Activity{
             pageToLogin();
         }
     }
-
+    /**
+         * Name:           pageToSummary
+        * Purpose:        Change page when print summary button clicked.
+        * Description:  Go to summary page.
+        */
     public void pageToSummary(View view) {
         Intent getScreen = new Intent(this, CusSummaryActivity.class);
         final int result = 1;
@@ -174,25 +227,44 @@ public class CustomerAccountActivity extends Activity{
         finish();
     }
 
+    /**
+         * Name:           pageToTransfer
+         * Purpose:        Change page when make transfer button clicked.
+         * Description:  Go to Make transfer page.
+         */
     public void pageToTransfer(View view) {
         Intent getScreen = new Intent(this, TransferChoiceActivity.class);
         final int result = 1;
         startActivityForResult(getScreen, result);
         finish();
     }
-
+    /**
+        * Name:          pageToLogin
+        * Purpose:       Go back to login page.
+        * Description:  Go to login page.
+        */
     public void pageToLogin() {
         Intent getScreen = new Intent(this, LoginActivity.class);
         final int result = 1;
         startActivityForResult(getScreen, result);
         finish();
     }
+
+    /**
+         * Name:          onBackPresse
+        * Purpose:       Enforce the customer to logout and go to login page.
+        * Description:  Enforce the customer to logout and go to login page.
+        */
     public void onBackPressed()
     {
         ParseUser.logOut();
         pageToLogin();
     }
-
+    /**
+        * Name:          onOptionsItemSelected
+        *   Purpose:     Enforce the customer to logout and go to login page.
+        * Description:  Enforce the customer to logout and go to login page.
+        */
     public boolean onOptionsItemSelected(MenuItem item){
         ParseUser.logOut();
         pageToLogin();
--- app/src/main/java/rmomoko/cse110bank/EmployeeModifiedCus.java
+++ app/src/main/java/rmomoko/cse110bank/EmployeeModifiedCus.java
@@ -1,3 +1,11 @@
+/**
+ * Team Name: Orange Chicken
+ *  File Name: EmployeeModifiedCus.java
+ *  Description: Display Customer's account information when teller login.
+ *
+ */
+
+
 package rmomoko.cse110bank;
 
 import android.content.Intent;
@@ -28,20 +36,40 @@ import rmomoko.cse110bank.Object.CheckingAccount;
 import rmomoko.cse110bank.Object.SavingAccount;
 
 /**
- * Created by yenhsialin on 10/25/2014.
+ * Name:            EmployeeModifiedCus
+ * Purpose:        Display customer's account information when teller login and search for customer.
+ * Description:  Get the customer's checking and saving balance from database.
+ *                      Create tell's activity button to process transactions, and perform the activity.
+ *
  */
 public class EmployeeModifiedCus extends Activity{
+    /* 30 days period to count interest and penalty*/
     private static final long THIRTYDAY = 2592000000L;
+
+    /* TextView to display customer's account balance */
     private TextView checkingNumber;
     private TextView savingNumber;
+
+    /* String to stores the customer's email */
     private String someoneEmail;
+
+    /* Data type from Parse */
     private User someone;
     private CheckingAccount userCheckAccount;
     private SavingAccount userSaveAccount;
+
+    /* Decimal to display amount */
     private DecimalFormat f = new DecimalFormat("##.00");
 
+    /**
+        * Name:           OnCreate
+        * Purpose:        Create the layout of Customer Account Information page
+        * Description:  Create the UI of CustomerAccountActivity page.
+        *                      Get data from database to display the amount of accounts.
+        *                      Create all the buttons to do all the customer account activities from teller.
+        */
     public void onCreate(Bundle savedInstanceState){
-
+        /* Create the layout of Tell's action page*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_change_account);
         getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -51,18 +79,26 @@ public class EmployeeModifiedCus extends Activity{
         someoneEmail = getIntent().getStringExtra("someoneEmail");
 
         ParseQuery<ParseUser> query = ParseUser.getQuery();
+
+        /* Get user information by finding the email */
         query.whereEqualTo("email",someoneEmail);
+        /* have the user's account information */
         query.include("CheckingAccount");
         query.include("SavingAccount");
         query.findInBackground(new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> parseUsers, ParseException e) {
+                /* If the customer exist */
                 if(e == null && !parseUsers.isEmpty())
                 {
+                    /* get user information */
                     someone = (User)parseUsers.get(0);
+                    /* get account information */
                     userCheckAccount = someone.getCheckingAccount();
                     userSaveAccount = someone.getSavingAccount();
+                    /* check if account closed */
                     if(!userCheckAccount.isClosed())
+                        /* show checking account balance */
                         checkingNumber.setText("$ " + f.format(userCheckAccount.getBalance()));
                     else
                         checkingNumber.setText("Account Closed");
@@ -78,59 +114,82 @@ public class EmployeeModifiedCus extends Activity{
             }
         });
 
+        /* create credit button */
         Button creditButton = (Button) findViewById(R.id.credit_button);
         creditButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(someone != null) {
+                    /* Change page to credit */
                     pageToCredit(view);
                 }
             }
         });
 
+        /* create debit button */
         Button debitButton = (Button) findViewById(R.id.debit_button);
         debitButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(someone != null) {
+                    /* Change page to dedit */
                     pageToDebit(view);
                 }
             }
         });
+
+        /* create checking account  penalty button */
         Button checkPenaltyButton = (Button) findViewById(R.id.check_penalty_button);
         checkPenaltyButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                /* user's accoun not closed */
                 if(!userCheckAccount.isClosed()) {
                     userCheckAccount.put("isClosed", false);
                     userCheckAccount.saveInBackground();
+
+                    /* check the penalty every 30 days */
                     if(userCheckAccount.getUpdatedAt().getTime() - userCheckAccount.getDate("lastTimePenalty").getTime() > THIRTYDAY)
                     {
                         double ave;
+                        /* if the user does not have account activity */
                         if(userCheckAccount.getHistory().isEmpty()) {
+                            /* get the balance */
                             ave = userCheckAccount.getBalance();
                         }
                         else
                         {
+                            /* get the last time balance */
                             ave = userCheckAccount.average();
                         }
+                        /* check account balance less than 100 */
                         if(ave < 100)
                         {
+                            /* apply penalty on current balance */
                             double current = userCheckAccount.getBalance();
                             userCheckAccount.put("balance", current - 25.0);
+
+                            /* update the time of penalty */
                             userCheckAccount.put("lastTimePenalty", userCheckAccount.getUpdatedAt());
                             userCheckAccount.saveInBackground();
+
+                            /* update penalty time */
                             Date currentTime = userCheckAccount.getUpdatedAt();
+
+                            /* string to show penalty history on customer's account summary */
                             String temp = userCheckAccount.getHistory();
                             temp = (currentTime.getYear()+ 1900) + "/" + (currentTime.getMonth()+1) + "/" + currentTime.getDate() + " "
                                     + f.format(userCheckAccount.getBalance()) + " penalty " + f.format(25) + "\n" +temp ;
                             userCheckAccount.put("history", temp);
                             userCheckAccount.saveInBackground();
+
+                            /* reset the checking account amount */
                             checkingNumber.setText("$ " + f.format((current - 25)));
                             Toast.makeText(EmployeeModifiedCus.this, "Penalty Apply", Toast.LENGTH_SHORT).show();
                         }
                         else
                         {
+                            /* no penalty applied, update the penalty check time */
                             userCheckAccount.put("lastTimePenalty", userCheckAccount.getUpdatedAt());
                             userCheckAccount.saveInBackground();
                             Toast.makeText(EmployeeModifiedCus.this, "No need to penalty within 30 days", Toast.LENGTH_SHORT).show();
@@ -144,7 +203,7 @@ public class EmployeeModifiedCus extends Activity{
             }
         });
 
-
+        /* create saving account  penalty button (( same as checking account penalty )) */
         Button savePenaltyButton = (Button) findViewById(R.id.save_penalty_button);
         savePenaltyButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -192,68 +251,95 @@ public class EmployeeModifiedCus extends Activity{
             }
         });
 
+         /* create checking account  interest button */
         Button checkInterestButton = (Button) findViewById(R.id.check_interest_button);
         checkInterestButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                /* user's account not closed */
                 if(!userCheckAccount.isClosed()) {
                     userCheckAccount.put("isClosed", false);
                     userCheckAccount.saveInBackground();
+
+                    /* check the interest every 30 days */
                     if(userCheckAccount.getUpdatedAt().getTime() - userCheckAccount.getDate("lastTimeInterest").getTime() > THIRTYDAY)
                     {
                         double ave;
+
+                        /* user did not have activities in the 30 days */
                         if(userCheckAccount.getHistory().isEmpty()) {
+                            /* get the balance directly */
                             ave = userCheckAccount.getBalance();
                         }
                         else
                         {
+                            /* get average amount in 30 days */
                             ave = userCheckAccount.average();
                         }
+                        /* check the average amount range */
                         if(ave >= 1000 && ave < 2000)
                         {
                             double current = userCheckAccount.getBalance();
+                            /* apply 1% interest to checking accoun balance */
                             userCheckAccount.put("balance", current + ave * 0.01);
+                            /* update the interest apply date */
                             userCheckAccount.put("lastTimeInterest", userCheckAccount.getUpdatedAt());
                             userCheckAccount.saveInBackground();
                             Date currentTime = userCheckAccount.getUpdatedAt();
+
+                            /* String to update in customer's account summary */
                             String temp = userCheckAccount.getHistory();
                             temp = (currentTime.getYear()+ 1900) + "/" + (currentTime.getMonth()+1) + "/" + currentTime.getDate() + " "
                                     + f.format(userCheckAccount.getBalance()) + " Interest " + f.format(ave * 0.01) + "\n"+temp ;
                             userCheckAccount.put("history", temp);
                             userCheckAccount.saveInBackground();
+                            /* update the checking account amount */
                             checkingNumber.setText("$ " + f.format((current + ave * 0.01)));
                             Toast.makeText(EmployeeModifiedCus.this, "Interest Apply", Toast.LENGTH_SHORT).show();
                         }
+                        /* check the average amount range */
                         else if(ave >= 2000 && ave < 3000)
                         {
                             double current = userCheckAccount.getBalance();
+                            /* apply 2% interest to checking account balance */
                             userCheckAccount.put("balance", current + ave * 0.02);
+                            /* update the interest apply date */
                             userCheckAccount.put("lastTimeInterest", userCheckAccount.getUpdatedAt());
                             userCheckAccount.saveInBackground();
                             Date currentTime = userCheckAccount.getUpdatedAt();
+
+                            /* String to update in customer's account summary */
                             String temp = userCheckAccount.getHistory();
                             temp = (currentTime.getYear()+ 1900) + "/" + (currentTime.getMonth()+1) + "/" + currentTime.getDate() + " "
                                     + f.format(userCheckAccount.getBalance()) + " Interest " + f.format(ave * 0.02) + "\n"+temp ;
                             userCheckAccount.put("history", temp);
                             userCheckAccount.saveInBackground();
+                            /* update the checking account amount */
                             checkingNumber.setText("$ " + f.format((current + ave * 0.02)));
                             Toast.makeText(EmployeeModifiedCus.this, "Interest Apply", Toast.LENGTH_SHORT).show();
                         }
+                        /* check the average amount range */
                         else if(ave >= 3000)
                         {
                             double current = userCheckAccount.getBalance();
+                            /* apply 3% interest to checking account balance */
                             userCheckAccount.put("balance", current + ave * 0.03);
+                            /* update the interest apply date */
                             userCheckAccount.put("lastTimeInterest", userCheckAccount.getUpdatedAt());
                             userCheckAccount.saveInBackground();
                             Date currentTime = userCheckAccount.getUpdatedAt();
+
+                            /* String to update in customer's account summary */
                             String temp = userCheckAccount.getHistory();
                             temp = (currentTime.getYear()+ 1900) + "/" + (currentTime.getMonth()+1) + "/" + currentTime.getDate() + " "
                                     + f.format(userCheckAccount.getBalance()) + " Interest " + f.format(ave * 0.03) + "\n"+temp ;
                             userCheckAccount.put("history", temp);
                             userCheckAccount.saveInBackground();
+                            /* update the checking account amount */
                             checkingNumber.setText("$ " + f.format((current + ave * 0.03)));
                             Toast.makeText(EmployeeModifiedCus.this, "Interest Apply", Toast.LENGTH_SHORT).show();
                         }
+                        /* low balance, so no interest apply */
                         else
                         {
                             userCheckAccount.put("lastTimeInterest", userCheckAccount.getUpdatedAt());
@@ -261,6 +347,7 @@ public class EmployeeModifiedCus extends Activity{
                             Toast.makeText(EmployeeModifiedCus.this, "No need to Interest within 30 days", Toast.LENGTH_SHORT).show();
                         }
                     }
+                    /* apply interest every 30 days */
                     else
                         Toast.makeText(EmployeeModifiedCus.this, "Already did the Interest within 30 days", Toast.LENGTH_SHORT).show();
                 }
@@ -269,7 +356,7 @@ public class EmployeeModifiedCus extends Activity{
             }
         });
 
-
+        /* create saving account  interest button (( same as checking account interest )) */
         Button saveInterestButton = (Button) findViewById(R.id.save_interest_button);
         saveInterestButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -348,13 +435,11 @@ public class EmployeeModifiedCus extends Activity{
         });
     }
 
-
-
-
-
-
-
-
+    /**
+        * Name:             pageToCredit
+        * Purpose:        Change page when print credit button clicked.
+        * Description:  Go to credit account page.
+        */
     public void pageToCredit(View view) {
         Intent getScreen = new Intent(this, CreditActivity.class);
         final int result = 1;
@@ -362,6 +447,12 @@ public class EmployeeModifiedCus extends Activity{
         startActivityForResult(getScreen, result);
         finish();
     }
+
+    /**
+        * Name:             pageToDredit
+        * Purpose:        Change page when print dredit button clicked.
+        * Description:  Go to dredit account page.
+        */
     public void pageToDebit(View view) {
         Intent getScreen = new Intent(this, DebitActivity.class);
         final int result = 1;
@@ -369,7 +460,11 @@ public class EmployeeModifiedCus extends Activity{
         startActivityForResult(getScreen, result);
         finish();
     }
-
+    /**
+         * Name:           pageToChoose
+         * Purpose:        Change page to search for customer when customer's account closed.
+         * Description:  Go to search for customer page.
+        */
     public void pageToChoose() {
 
         Intent getScreen = new Intent(this, EmployeeChooseCus.class);
